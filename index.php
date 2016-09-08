52cda47163eff8c4e0909ab1a8cc346b4e7cafb2
celso andre <celsolog2012@gmail.com>
2016-09-07 21:58:00 -0300

    Constantes

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-    // Globais
-    print_r($_SERVER);
+    //Constantes
+
+    define("URL","http://www.meusite.com.br");
+    define("VERSION", "1.0");
+    
+    echo "meu site eh ".URL." ". VERSION ;
+  
     
 ?>
