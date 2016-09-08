a6a98004e0cbd94c051dd93c0872b35ff3fccb16
celso andre <celsolog2012@gmail.com>
2016-09-07 21:53:23 -0300

    Variáveis Globais

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
-    //Declando Compostas
-    $grupos = array(1,2,3);
-    
-    $variavel = array(
-        "nome"=>"celso",
-        "sobrenome"=>31,
-        "sexo"=>"masculino"
-    );
-    
-    echo $variavel["sexo"];
-    echo $grupos[1];
-    
-    print_r($grupos);
+    // Globais
+    print_r($_SERVER);
     
 ?>
