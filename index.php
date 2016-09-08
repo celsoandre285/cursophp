675c3b63020d13af5a38e6201fa15a72c9f98514
celso andre <celsolog2012@gmail.com>
2016-09-07 21:07:30 -0300

    Sintaxe de códigos PHP

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>teste PHP</title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+    /*Sintaxe de códigos PHP*/
+    $aluno = "celso";#declarando String
+   
+
+?>
